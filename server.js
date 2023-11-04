46cd49a110e91b502def7eb0ee1ed13b33ff7b9c
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-04 10:50:49 -0500

    added middleware and fixed typo

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const path = require('path');
 //initializing express app
 const app = express();
 
+const notesCollection = require('./Develop/db/db.json')
+
 //giving option of using env port info from Heroku or my hardcoded port at 3001
 const port = process.envPORT || 3001;
 
 //Setting up static middleware to allow access to public forled to deliver static files
 app.use(express.static('public'));
+//Setting up URL middleware
+app.use(express.urlencoded({extended:true}));
+//Setting up Middleware to allow JS objects to be passed
+app.use(express.json());
 
 
 
 
 
-//one time function to have app start lietneing to previously indicated port.
+//one time function to have app start listening to previously indicated port options.
 app.listen(PORT, () =>    
     console.log('Note Taker listening at http://localhost:${PORT}')
     );
