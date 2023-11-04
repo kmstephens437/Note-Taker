4db7b92bf5eb674a86d461b48743c233909256c9
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-04 10:34:44 -0500

    Initial code for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+//requiring express and path packages
+const express = require('express');
+const path = require('path');
+
+//initializing express app
+const app = express();
+
+//Hardcoding port tp 3001
+const port = 3001;
+
+//Setrting up static middleware to allow access to public forled to deliver static files
+app.use(express.static('public'));
+
+
+
+
+
+//one time function to have app start lietneing to previously indicated port.
+app.listen(PORT, ( =>    
+    console.log('Note Taker listening at http://localhost:${PORT}')
+    ));
