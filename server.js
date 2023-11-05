1c0f3011785305bd817ba9e3b4c9b29e3bcd79c0
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-04 21:05:58 -0500

    corrected path for notes/index files.

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 //Setting up Middleware to use API
 
 app.get('/notes', (req,res) =>
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
 );
 
 app.get('/',(req,res) =>
-res.sendFile(path.join(__dirname, '../public/index.html'))
+res.sendFile(path.join(__dirname,'/public/index.html'))
 );
 
 
